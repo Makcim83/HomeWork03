541398e48f0e8005bfb8e91365750a989882d072
admin <mmd83@63.ru>
2022-11-10 10:00:29 +0400

    Домашнее задание HW3 на проверку

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         long peoplesIndia=1500000000L;
         float someFloatNumber=4.5f;
         double smallNumber=0.000004;
-        System.out.println("Part " + homeWorkPart);
+        System.out.println("HomeWork 3 Part " + homeWorkPart);
         System.out.println("Всего целочисленных переменных " + wholeNumberTypes + " типа");
         System.out.println("В двух байтах умещается более " + twoBytes + " значений, но и этого бывает мало");
         System.out.println("Чтобы рассказать о численности населения Индии понадобится 4 байта, там проживает более " + peoplesIndia + " человек");
@@ -77,12 +77,74 @@ public class Main {
         BottlesSiries = averageBottlesPerMinute * workingMonths * minutesInOneHour * hoursInOneDay * daysInMonth;
         System.out.println("За время " + workingMonths + " мес. произведется " + BottlesSiries + " бутылок");
 
+        //Part 5
+        homeWorkPart +=1;
+        System.out.println("Part " + homeWorkPart);
+        int paintsCanAmount = 120;
+        int requireWhiteCan = 2;
+        int requireBrowhCan = 4;
+        int requirePaintsCan = requireWhiteCan + requireBrowhCan;
+        int classRooms = paintsCanAmount / requirePaintsCan;
+        int needWhiteCanAmount = classRooms * requireWhiteCan;
+        int needBrownCanAmount = classRooms * requireBrowhCan;
+        System.out.println("В школе, где " + classRooms + " классов нужно " + needWhiteCanAmount + " банок белой краски и " + needBrownCanAmount + " банок коричневой краски");
 
+        //Part 6
+        homeWorkPart +=1;
+        System.out.println("Part " + homeWorkPart);
+        int bananaPcs = 5;
+        int milkVolume = 200;
+        int iceCreamPcs = 2;
+        int eggPcs = 4;
 
+        int bananaWeightEach = 80;
+        float milkSpecificVolume = 1.05f;
+        int iceCreamWeightEach = 100;
+        int eggWeightEach = 70;
 
+        int bananaWeightAmount = bananaPcs * bananaWeightEach;
+        float milkWeightAmount = milkVolume * milkSpecificVolume;
+        int iceCreamAmount = iceCreamPcs * iceCreamWeightEach;
+        int eggWeightAmount = eggPcs * eggWeightEach;
+        float dinnerWeightAmount = bananaWeightAmount + milkWeightAmount + iceCreamAmount + eggWeightAmount;
+        float dinnerWeightAmountKg = dinnerWeightAmount / 1000;
 
+        System.out.println("Вес спорт-завтрака составляет " + dinnerWeightAmount + "г. (" + dinnerWeightAmountKg + "кг)");
 
+        //Part 7
+        homeWorkPart +=1;
+        System.out.println("Part " + homeWorkPart);
+        int dropWeightAmountKg = 7;
+        int dropEveryDayDietA = 250;
+        int dropEveryDayDietB = 500;
+        float dropG = dropWeightAmountKg * 1000;
 
+        float daysDietA = dropG / dropEveryDayDietA;
+        float daysDietB = dropG / dropEveryDayDietB;
+        System.out.println("Для сброса веса " + dropWeightAmountKg + " кг спортсмену понадобится сидеть на диете А " + daysDietA + " дней или " + daysDietB + " дней на диете типа В.");
 
+        //Part 8
+        homeWorkPart +=1;
+        System.out.println("Part " + homeWorkPart);
+        int beforeSalaryMasha = 67760;
+        int beforeSalaryDenis = 83690;
+        int beforeSalaryKristina = 76230;
+        float growSalaryPercent = 10;
+
+        System.out.println("Отчет по результатам ежегодного повышения зарплаты на " + growSalaryPercent + "% сотрудникам со стажем от 3х лет:");
+        float addSalaryMasha = beforeSalaryMasha * growSalaryPercent / 100;
+        float afterSalaryMasha = beforeSalaryMasha + addSalaryMasha;
+        float addYearSalaryMasha = addSalaryMasha * 12;
+        System.out.println("Маша теперь получает " + afterSalaryMasha + " рублей в месяц, годовой доход вырос на " + addYearSalaryMasha + " рублей");
+
+        float addSalaryDenis = beforeSalaryDenis * growSalaryPercent / 100;
+        float afterSalaryDenis = beforeSalaryDenis + addSalaryDenis;
+        float addYearSalaryDenis = addSalaryDenis * 12;
+        System.out.println("Денис теперь получает " + afterSalaryDenis + " рублей в месяц, годовой доход вырос на " + addYearSalaryDenis + " рублей");
+
+        float addSalaryKristina = beforeSalaryKristina * growSalaryPercent / 100;
+        float afterSalaryKristina = beforeSalaryKristina + addSalaryKristina;
+        float addYearSalaryKristina = addSalaryKristina * 12;
+        System.out.println("Кристина теперь получает " + afterSalaryKristina + " рублей в месяц, годовой доход вырос на " + addYearSalaryKristina + " рублей");
     }
 }
